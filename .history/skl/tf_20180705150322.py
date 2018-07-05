6a4d921f390cdc818fc27a4b26488718380273ce
yangkai <sygtxyj@gmail.com>
2018-07-05 21:07:52 +0800

    'git'

---
+++ .history/skl/tf_20180705150322.py
@@ -0,0 +1,19 @@
+import pdb
+from sklearn.feature_extraction.text import CountVectorizer  
+from sklearn.feature_extraction.text import TfidfTransformer
+
+tag_list = ['青年 吃货 唱歌',  
+            '少年 游戏 叛逆',  
+            '少年 吃货 足球'] 
+
+vectorizer = CountVectorizer() #将文本中的词语转换为词频矩阵  
+X = vectorizer.fit_transform(tag_list) #计算个词语出现的次数
+pdb.set_trace()
+"""
+word_dict = vectorizer.vocabulary_
+{'唱歌': 2, '吃货': 1, '青年': 6, '足球': 5, '叛逆': 0, '少年': 3, '游戏': 4}
+"""
+
+transformer = TfidfTransformer()  
+tfidf = transformer.fit_transform(X)  #将词频矩阵X统计成TF-IDF值  
+print(tfidf.toarray())
